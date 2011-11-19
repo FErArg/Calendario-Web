bdfe6805e999854a8902ca72e46f04a99b9dbe1e
SerInformaticos <info@serinformaticos.es>
2011-11-19 09:27:37 +0100

    Primer Envío - actualizado

--- index.php
+++ index.php
@@ -1,4 +1,38 @@
 <?php
+/****************************************************************************************
+#     Copyright (c) 2008, 2009, 2010, 2011 Fernando A. Rodriguez para SerInformaticos.es
+#
+#     Este programa es software libre: usted puede redistribuirlo y / o modificarlo
+#     bajo los términos de la GNU General Public License publicada por la
+#     la Free Software Foundation, bien de la versión 3 de la Licencia, o de
+#     la GPL2, o cualquier versión posterior.
+#
+#     Este programa se distribuye con la esperanza de que sea útil,
+#     pero SIN NINGUNA GARANTÍA, incluso sin la garantía implícita de
+#     COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR. Véase el
+#     GNU General Public License para más detalles.
+#
+#     Usted debería haber recibido una copia de la Licencia Pública General de GNU
+#     junto con este programa. Si no, visite <http://www.gnu.org/licenses/>.
+#
+#     Puede descargar la version completa de la GPL3 en este enlace: 
+#     	< http://www.serinformaticos.es/index.php?file=kop804.php >
+#
+#	 Para mas información puede contactarnos :
+#
+#		Teléfono (+34) 961 19 60 62
+#
+#		Email	info@serinformaticos.es
+#
+#		MSn:	info@serinformaticos.es
+#
+#		Twitter	@SerInformaticos
+#
+#		Web:	www.SerInformaticos.es
+#
+*****************************************************************************************/
+?>
+<?php
 $inc="inc";
 include("$inc/incluirI.php");
 ?>
